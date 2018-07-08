4c68c9ed46c92a89e7b8dfcbe34a1482fee6b88b
Jon Craton <jncraton@gmail.com>
2018-07-08 14:38:06 -0400

    Fixed second case

--- PROG1.pl
+++ PROG1.pl
@@ -6,6 +6,12 @@ my @lex_sort = sort @ARGV;
 my @num_sort = sort {$a <=> $b} @ARGV;
 my @rev = reverse @ARGV;
 
+my $second = "no second value";
+
+if ($len > 1) {
+  $second = @num_sort[1];
+}
+
 print "
 ********************* PROG 1 ********************
 ******* by Jon Craton ***************************
@@ -18,6 +24,6 @@ Reversed List of Elements : @rev
 Sorted \@ARGV by ASCII : @lex_sort
 Sorted \@ARGV by number : @num_sort
 Largest numeric value in \@ARGV : @num_sort[-1]
-Second element in \@ARGV : @num_sort[-2] {if none it should print 'no second value'}
+Second element in \@ARGV : $second
 ********************* THE END ******************
 "
